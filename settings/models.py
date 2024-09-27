39096993a6897ff3c6a10ac5b53cfca791959ddd
AdamTheThirst <chatbotsadmn@gmail.com>
2024-09-27 20:18:16 +0300

    add TimeStampModel template for all models

--- settings/models.py
+++ settings/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+class TimeStampedModel(models.Model):
+    '''Time stamps base template'''
+
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+
+    def __str__(self) -> str:
+        return str(f'{self.pk} — {self.created}')
