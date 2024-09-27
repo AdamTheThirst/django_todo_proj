4010c971ee0778695f51b21f2cab793c927fba88
AdamTheThirst <chatbotsadmn@gmail.com>
2024-09-27 21:17:01 +0300

    add main model fields

--- list_todo/models.py
+++ list_todo/models.py
@@ -1,3 +1,37 @@
 from django.db import models
+from django.utils import timezone
+
+from settings.models import TimeStampedModel
+
 
 # Create your models here.
+
+
+class ListTodo(TimeStampedModel):
+    '''TODO_ list'''
+
+    IN_PROGRESS = 'in progress'
+    DONE = 'done'
+    CANCELLED = 'cancelled'
+    DELAYED_START = 'delayed start'
+    WAIT_FOR_START = 'wait for start'
+
+    DONE_STATUS = (
+        (IN_PROGRESS, 'В работе'),
+        (DONE, 'Исполнено'),
+        (CANCELLED, 'Отменено'),
+        (DELAYED_START, 'Отложить начало'),
+        (WAIT_FOR_START, 'Ожидает начала')
+    )
+
+    task = models.CharField(max_length=200, null=False, blank=False)
+    task_description = models.TextField(default='', null=True, blank=True)
+    status = models.CharField(max_length=30, choices=DONE_STATUS, default=WAIT_FOR_START, null=False, blank=False)
+    start_time = models.DateTimeField(null=True, blank=True, default=timezone.now)
+
+    def __str__(self):
+        return self.task
+
+    class Meta:
+        verbose_name = 'Задача'
+        verbose_name_plural = 'Задачи'
