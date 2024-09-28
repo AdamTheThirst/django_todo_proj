cc11538f23481906e2804e275d3a864da1c0ec50
AdamTheThirst <chatbotsadmn@gmail.com>
2024-09-28 23:28:30 +0300

    create superuser and added model to admin.py

--- list_todo/admin.py
+++ list_todo/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
 
+from .models import ListTodo
+
 # Register your models here.
+
+@admin.register(ListTodo)
+class CustomListTodoAdmin(admin.ModelAdmin):
+    list_display = (
+        'task_kind',
+        'task',
+        'task_description',
+        'status',
+        'start_time',
+    )
+
+    list_filter = (
+        'task_kind',
+        'task',
+        'status',
+        'start_time',
+    )
