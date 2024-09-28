5930bdaeb11f9f25ea846cd9c690ef938f0b5d15
AdamTheThirst <chatbotsadmn@gmail.com>
2024-09-28 23:27:47 +0300

    modifed fields of tasks table

--- list_todo/models.py
+++ list_todo/models.py
@@ -24,10 +24,26 @@ class ListTodo(TimeStampedModel):
         (WAIT_FOR_START, 'Ожидает начала')
     )
 
-    task = models.CharField(max_length=200, null=False, blank=False)
-    task_description = models.TextField(default='', null=True, blank=True)
-    status = models.CharField(max_length=30, choices=DONE_STATUS, default=WAIT_FOR_START, null=False, blank=False)
-    start_time = models.DateTimeField(null=True, blank=True, default=timezone.now)
+    PERSONAL = 'personal'
+    WORK = 'work'
+    EDUCATION = 'education'
+    KIDS = 'kids'
+    OTHER = 'other'
+
+    TASK_KINDS = (
+        (PERSONAL, 'личное'),
+        (WORK, 'работа'),
+        (EDUCATION, 'обучеие'),
+        (KIDS, 'дети'),
+        (OTHER, 'иное'),
+    )
+
+    task = models.CharField(max_length=200, null=False, blank=False, verbose_name='Задача')
+    task_description = models.TextField(default='', null=True, blank=True, verbose_name='Пояснение')
+    task_kind = models.CharField(max_length=20, choices=TASK_KINDS, null=False, blank=False, default='PERSONAL', verbose_name='Тип задачи')
+    status = models.CharField(max_length=30, choices=DONE_STATUS, default=WAIT_FOR_START, null=False, blank=False, verbose_name='Статус')
+    start_time = models.DateTimeField(null=True, blank=True, default=timezone.now, verbose_name='Время начала')
+    deadline = models.DateTimeField(null=True, blank=True, verbose_name='Дэдлайн')
 
     def __str__(self):
         return self.task
