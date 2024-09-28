f7dae2ed01da3dba13c547e20f33970d0a1690e0
AdamTheThirst <chatbotsadmn@gmail.com>
2024-09-28 23:41:37 +0300

    create base view

--- list_todo/views.py
+++ list_todo/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
+from .models import ListTodo
+
 # Create your views here.
+
+def index(request):
+    qs = ListTodo.objects.all()
+    contest = {'objects_list': qs}
+    return render(request, 'list_todo/index.html', contest)
